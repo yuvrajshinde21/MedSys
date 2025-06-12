c280daa4b75d70c14e6c652197b7b8e4ad14e543
AbhijeetJadhav12 <jabhijeet0143@gmail.com>
2025-06-12 18:09:02 +0530

    added a few pages

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -5,15 +5,26 @@ exports.getLoginPage = function (req, res) {
     res.render("login.ejs");
 }
 
-// @desc login
-// @route POST: "auth/login"
+exports.getRegisterDoctorPage = function (req, res) {
+    res.render("registerDoctor.ejs", { partialToInclude: true });
+}
+
+exports.getViewDoctorsPage = function (req, res) {
+    res.render("viewDoctors.ejs", { partialToInclude: true });
+}
+
+exports.getRegisterReceptionistPage = function (req, res) {
+    res.render("registerReception.ejs", { partialToInclude: true });
+}
+// @desc register doctor
+// @route POST: "auth/register/doctor"
 // @access public
 exports.handleLogin = function (req, res) {
     const { username, password, role } = req.body;
 
     if (role === "admin") {
         res.render("adminDashboard", { partialToInclude: false });
-    } else if (role === "reception") {
+    } else if (role === "Receptionist") {
         res.render("receptionDashboard", { partialToInclude: false });
     } else if (role === "doctor") {
         res.render("adminDashboard", { partialToInclude: false });
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
-const { getLoginPage, handleLogin } = require("../controllers/authController")
+const { getLoginPage, handleLogin, getRegisterDoctorPage, getViewDoctorsPage, getRegisterReceptionistPage } = require("../controllers/authController")
 
 router.route("/login").get(getLoginPage).post(handleLogin);
-
+router.route("/addDoctor").get(getRegisterDoctorPage);
+router.route("/viewDoctors").get(getViewDoctorsPage);
+router.route("/registerReception").get(getRegisterReceptionistPage)
+// router.route("/register/receptionist").post(handleRegisterReceptionist);
+// router.route("/register/doctor").post(handleRegisterDoctor);
 module.exports = router;
